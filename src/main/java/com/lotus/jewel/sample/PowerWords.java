2957070dc532619f7d24f1405721e5dbae861fbd
Warbler14 <firefly11a@naver.com>
2021-09-14 00:14:30 +0900

    The_power_words

---
+++ src/main/java/com/lotus/jewel/sample/PowerWords.java
@@ -0,0 +1,13 @@
+package com.lotus.jewel.sample;
+
+public class PowerWords {
+
+	private String [] powerWords = {
+			"힘은 절실한 순간에"
+			, "막힘이 오더라도 버둥거리면 풀려나간다"
+	};
+	
+	public String[] getPowerWords () {
+		return powerWords;
+	}
+}
